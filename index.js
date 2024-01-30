c5c8d7f52b9d2de010e1182105921805ccb52d93
joyasturay <joyasturay1@gmail.com>
2024-01-31 03:42:40 +0530

    phase one part b

--- index.js
+++ index.js
@@ -3,12 +3,14 @@ const app=express();
 const mongoose=require("mongoose");
 const path=require("path");
 const Listing=require("./models/listings.js");
+const ejsMate=require("ejs-mate");
 var methodOverride = require('method-override');
 app.set("view engine","ejs");
 app.set("views",path.join(__dirname,"views"));
-app.use(express.static(path.join(__dirname,"public")));
+app.use(express.static(path.join(__dirname,"/public")));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
+app.engine('ejs', ejsMate);
 
 const Mongo_url="mongodb://127.0.0.1:27017/wanderlust";
 
